e84fdd10955cad19e6b136eed942ad8181eb2bd9
Kboutou <kabil.boutou@gmail.com>
2024-10-04 12:27:54 +0200

    refacto : change color bars

--- src/Components/chart.tsx
+++ src/Components/chart.tsx
@@ -1,24 +1,45 @@
 import React from 'react'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell } from 'recharts'
 
 interface Props {
-  data: Array<{ name: string; perc: number }>
-  barLabels: Array<{ label: string; fill: string }>
+  data: Array<{ name: string; value: number; fill: string }>
 }
 
-export function CustomBarChart({ data, barLabels }: Props) {
+export function CustomBarChart({ data }: Props) {
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+    <ResponsiveContainer height={300}>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip />
-        <Legend />
-        {barLabels.map((barLabel) => (
-          <Bar dataKey={barLabel.label} fill={barLabel.fill} />
-        ))}
+        <Bar dataKey="value" shape={<TriangleBar />} label={{ position: 'top' }}>
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={entry.fill} />
+          ))}
+        </Bar>
       </BarChart>
     </ResponsiveContainer>
   )
+}
+
+function getPath(x, y, width, height) {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`
+}
+
+function TriangleBar(props) {
+  const { fill, x, y, width, height } = props
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />
 }
--- src/consts/queryKeys.ts
+++ src/consts/queryKeys.ts
@@ -1,1 +1,15 @@
 export const ENERGY_DATA_GET = 'ENERGY_DATA_GET'
+export const CHART_COLORS = [
+  '#FF5733', // Red-Orange
+  '#33FF57', // Green
+  '#3357FF', // Blue
+  '#FF33A1', // Pink
+  '#00000', // Black
+  '#33FFF6', // Cyan
+  '#FF9F33', // Orange
+  '#8D33FF', // Purple
+  '#FF3380', // Hot Pink
+  '#33FF9F', // Mint Green
+  '#FF5733', // Coral
+  '#339FFF', // Light Blue
+]
